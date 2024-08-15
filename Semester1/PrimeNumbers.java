800692c5115c8da896c56aae9302ff1413b8d826
Shininden <mathpaz.oliveira@gmail.com>
2024-08-15 10:14:46 -0300

    Initial commit

---
+++ Semester1/PrimeNumbers.java
@@ -0,0 +1,35 @@
+package Semester1;
+public class PrimeNumbers 
+{
+    public static void main(String[] args) 
+    {
+        int n = 2, tot_Of_Primes = 0;
+
+        System.out.print("2 ");
+
+        do 
+        {
+            int divisor = 2;
+            int halfway = n / 2;
+            boolean isPrime = true;
+
+            do 
+            {
+                if(n % divisor == 0)
+                    isPrime = false;
+
+                divisor++;
+            } 
+            while (divisor <= halfway && isPrime);
+
+
+            if(isPrime)
+            {
+                System.out.print(n + " ");
+                tot_Of_Primes++;
+            }
+            n++;
+        } 
+        while (tot_Of_Primes <= 100);
+    }
+}
