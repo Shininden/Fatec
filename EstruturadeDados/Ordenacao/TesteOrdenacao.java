e3c7c5362e4cd8df19fee30ad850bdd319000ac2
Matheus Paz Oliveira <mathpaz.oliveira@gmail.com>
2025-06-12 18:46:20 -0300

    Update TesteOrdenacao.java

--- EstruturadeDados/Ordenacao/TesteOrdenacao.java
+++ EstruturadeDados/Ordenacao/TesteOrdenacao.java
@@ -5,29 +5,20 @@ import Fatec.EstruturadeDados.Pilha.NossoVetor;
 
 import java.util.Date;
 
-public class TesteOrdenacao {
-    public static void main(String[] args) {
+public class TesteOrdenacao
+{
+    public static void main(String[] args) 
+    {
         Scanner scanner = new Scanner(System.in);
         int t;
         System.out.print("digite o tamanho do vetor, 0 encerra: ");
         t = scanner.nextInt();
-        while (t > 0) {
+        
+        while (t > 0) 
+        {
             long ini, fim;
             NossoVetor v = new NossoVetor(t);
-            //v.preencheVetor();
-            //System.out.println("vetor original\n" + v);
-            // Date d = new Date();
-            // ini = d.getTime();
-            // v.bubbleSort();
-            // fim = new Date().getTime();
-            // System.out.println("bubble demorou " + (fim-ini) + " milissegundos");
-            //System.out.println("vetor ordenado pelo bubble\n" + v);
 
-            // v.preencheVetor();
-            // ini = new Date().getTime();            
-            // v.insertionSort();
-            // fim = new Date().getTime();
-            // System.out.println("insertion demorou " + (fim - ini) + " milissegundos");
 
             v.autoPreencheVetor();
             ini = new Date().getTime();
@@ -43,15 +34,6 @@ public class TesteOrdenacao {
             System.out.println("quick ordenado demorou " + (fim - ini) + " milissegundos");
 
 
-            // ini = new Date().getTime();
-            // v.bubbleSort();
-            // fim = new Date().getTime();
-            // System.out.println("bubble ordenado demorou " + (fim-ini) + " milissegundos");
-            
-            // ini = new Date().getTime();
-            // v.insertionSort();
-            // fim = new Date().getTime();
-            // System.out.println("insertion ordenado demorou " + (fim-ini) + " milissegundos");
             
             System.out.print("digite o novo tamanho, 0 encerra: ");
             t = scanner.nextInt();
